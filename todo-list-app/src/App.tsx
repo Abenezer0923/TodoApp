d778c23ffe7f183ff3cfc99ccda83d65e09ffdae
abniNew <abnew0923@gmail.com>
2023-09-18 11:05:18 -0700

    I add Router in this commite

--- todo-list-app/src/App.tsx
+++ todo-list-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import Lists from './components/List';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './Pages/Home'; // Correct import path
+import About from './Pages/About'; // Correct import path
 
-
-
-const App: React.FC =() =>  {
+const App: React.FC = () => {
   const [todos, setTodos] = useState<string[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
 
@@ -26,19 +27,30 @@ const App: React.FC =() =>  {
     setTodos(updatedTodos);
   };
 
-
   return (
-    <div className="App">
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add</button>
-      <Lists todos={todos} onDelete={deleteTodo} onEdit={editTodo} />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Todo List</h1>
+        <nav>
+          <Link to="/home">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+        <Routes>
+        <Route
+          path="/"
+          element={<Lists todos={todos} onDelete={deleteTodo} onEdit={editTodo} />}
+        />
+        <Route path="/about" element={<About />}  />
+        <Route path='/home' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
